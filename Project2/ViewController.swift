dedf14930a8660edb3c9cffbd4ea2a15e30b3f77
Yash Tamakuwala <yash@yashs-macbook-pro-2.local>
2016-09-03 23:49:59 +0530

    Randomising the flags.

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import GameplayKit
 
 class ViewController: UIViewController {
 
@@ -16,11 +17,21 @@ class ViewController: UIViewController {
     
     var countries = [String]()
     var score = 0
+    var correctAnswer = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
         countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
         
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        button1.layer.borderColor = UIColor.lightGray.cgColor
+        button2.layer.borderColor = UIColor.lightGray.cgColor
+        button3.layer.borderColor = UIColor.lightGray.cgColor
+
+        askQuestion()
     }
 
     override func didReceiveMemoryWarning() {
@@ -28,6 +39,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    func askQuestion(){
+        countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
 
+        button1.setImage(UIImage(named : countries[0]), for: UIControlState.normal)
+        button2.setImage(UIImage(named : countries[1]), for: UIControlState.normal)
+        button3.setImage(UIImage(named : countries[2]), for: UIControlState.normal)
+        
+        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+    }
 }
 
