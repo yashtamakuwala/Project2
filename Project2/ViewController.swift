edf19485816883c62ce5a92f596d1a5f8dc220e7
Yash Tamakuwala <yash@yashs-macbook-pro-2.local>
2016-09-03 23:16:26 +0530

    Initial Commit

---
+++ Project2/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Project2
+//
+//  Created by Yash Tamakuwala on 03/09/16.
+//  Copyright © 2016 Yash Tamakuwala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        countries += ["estonia", "france", "germany", "ireland", "italy", "monaco", "nigeria", "poland", "russia", "spain", "uk", "us"]
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
