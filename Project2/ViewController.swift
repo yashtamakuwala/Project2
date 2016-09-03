20ce1b99869f68266a832b0c3886c2d71c8b0b91
Yash Tamakuwala <yash@yashs-macbook-pro-2.local>
2016-09-04 00:58:32 +0530

    Finished.

--- Project2/ViewController.swift
+++ Project2/ViewController.swift
@@ -40,14 +40,35 @@ class ViewController: UIViewController {
     }
 
     
-    func askQuestion(){
+    func askQuestion(action: UIAlertAction! = nil){
         countries = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: countries) as! [String]
 
         button1.setImage(UIImage(named : countries[0]), for: UIControlState.normal)
         button2.setImage(UIImage(named : countries[1]), for: UIControlState.normal)
         button3.setImage(UIImage(named : countries[2]), for: UIControlState.normal)
         
-        correctAnswer = GKRandomSource.sharedRandom().nextIntWithUpperBound(3)
+        correctAnswer = GKRandomSource.sharedRandom().nextInt(upperBound: 3)
+        title = countries[correctAnswer].uppercased()
     }
+    
+    @IBAction func buttonTapped(_ sender: AnyObject) {
+        
+        var title: String
+        
+        if sender.tag == correctAnswer {
+            title = "Correct"
+            score += 1
+        } else {
+            title = "Wrong"
+            score -= 1
+        }
+        
+        let alert = UIAlertController(title: title, message: "Your score is \(score)", preferredStyle: .alert)
+        let ok = UIAlertAction(title: "Continue", style: .default, handler: askQuestion) // we want the game to continue when the button is tapped, so we pass in askQuestion so that iOS will call our askQuestion() method.
+        
+        alert.addAction(ok)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
